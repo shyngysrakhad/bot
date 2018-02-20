b5ced4b9fde985d716ced502177ad9e85216604e
DevChinga <34274309+DevChinga@users.noreply.github.com>
2018-02-20 14:21:16 +0600

    сommit 1

--- bot.py
+++ bot.py
@@ -29,29 +29,29 @@ while True:
         elif response['items'][0]['body'] == 'ъ':
             write_msg(item['user_id'], 'Эти буквы не переводятся')
         else:
-            final = text.replace("я","i'a").replace("Я","I'a").replace("ю","i'y").replace("Ю","I'y").replace("ц","ts").replace("а", "a").replace("ә", "a'").replace("б", "b").replace("д", "d").replace("е",
+            final = text.replace("я","ıa").replace("Я","Ia").replace("ю","ıý").replace("Ю","Iý").replace("ц","ts").replace("а", "a").replace("ә", "á").replace("б", "b").replace("д", "d").replace("е",
                                                                                                                                                                                                     "e").replace(
-            "ф", "f").replace("г", "g").replace("ғ", "g'").replace("х", "h").replace("һ", "h").replace("і",
-                                                                                                       "i").replace(
-            "и", "i'").replace("й", "i'").replace("ж", "j").replace("к", "k").replace("л", "l").replace("м",
+            "ф", "f").replace("г", "g").replace("ғ", "ǵ").replace("х", "h").replace("һ", "h").replace("і",
+                                                                                                       "і").replace(
+            "и", "ı").replace("й", "ı").replace("ж", "j").replace("к", "k").replace("л", "l").replace("м",
                                                                                                         "m").replace(
-            "н", "n").replace("ң", "n'").replace("о", "o").replace("ө", "o'").replace("п", "p").replace("қ",
+            "н", "n").replace("ң", "ń").replace("о", "o").replace("ө", "ó").replace("п", "p").replace("қ",
                                                                                                         "q").replace(
-            "р", "r").replace("с", "s").replace("ш", "s'").replace("щ", "s'").replace("ч", "c'").replace("т",
+            "р", "r").replace("с", "s").replace("ш", "sh").replace("щ", "sh").replace("ч", "ch").replace("т",
                                                                                                          "t").replace(
-            "ұ", "u").replace("ү", "u'").replace("в", "v").replace("ы", "y").replace("у", "y'").replace("з",
+            "ұ", "u").replace("ү", "ú").replace("в", "v").replace("ы", "y").replace("у", "ý").replace("з",
                                                                                                         "z").replace(
-            "ь", "").replace("ъ", "").replace("Ъ", "").replace("Ь", "").replace("А", "A").replace("Ә", "A'").replace("Б", "B").replace("Д",
+            "ь", "").replace("ъ", "").replace("Ъ", "").replace("Ь", "").replace("А", "A").replace("Ә", "Á").replace("Б", "B").replace("Д",
                                                                                                                                        "D").replace(
-            "Е", "E").replace("Ф", "F").replace("Г", "G").replace("Ғ", "G'").replace("Х", "H").replace("І",
+            "Е", "E").replace("Ф", "F").replace("Г", "G").replace("Ғ", "Ǵ").replace("Х", "H").replace("І",
                                                                                                        "I").replace(
-            "Й", "I'").replace("И", "I'").replace("Ж", "J").replace("К", "K").replace("Л", "L").replace("М",
+            "Й", "I").replace("И", "I").replace("Ж", "J").replace("К", "K").replace("Л", "L").replace("М",
                                                                                                         "M").replace(
-            "Н", "N").replace("Ң", "N'").replace("О", "O").replace("Ө", "O'").replace("П", "P").replace("Қ",
+            "Н", "N").replace("Ң", "Ń").replace("О", "O").replace("Ө", "Ó").replace("П", "P").replace("Қ",
                                                                                                         "Q").replace(
-            "Р", "R").replace("С", "S").replace("Ш", "S'").replace("Щ", "S'").replace("Ч", "C'").replace("Т",
+            "Р", "R").replace("С", "S").replace("Ш", "Sh").replace("Щ", "Sh").replace("Ч", "Ch").replace("Т",
                                                                                                          "T").replace(
-            "Ұ", "U").replace("Ү", "U'").replace("В", "V").replace("Ы", "Y").replace("У", "Y'").replace("З", "Z").replace("ё", "е").replace("Ё", "E").replace("э","e").replace("Э","E")
+            "Ұ", "U").replace("Ү", "Ú").replace("В", "V").replace("Ы", "Y").replace("У", "Ý").replace("З", "Z").replace("ё", "е").replace("Ё", "E").replace("э","e").replace("Э","E")
             write_msg(item['user_id'], final)
 
 
